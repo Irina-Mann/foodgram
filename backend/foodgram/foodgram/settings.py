30a6566cf2e5024708c5d16225b113b50403591d
Irina-Mann <kio.gar@mail.ru>
2024-05-20 19:52:05 +0500

    debag

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -16,8 +16,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.getenv('DEBUG') == 'True'
+
+DEBUG = os.getenv('DEBUG') == 'True'
 
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,foodyam.zapto.org').split(',')
